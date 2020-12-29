df967d261e8e798d9245b6e2af351c0f54894e50
Enoch Qu <enochqu@outlook.com>
2020-12-29 00:25:15 -0800

    reset project

--- project/exams/exam01/words.js
+++
@@ -1,9 +0,0 @@
-// exports an array of valid words
-const wordList = `
-START TREES TREAT STARS TRAPS PARTS
-STRAP STARK CARTS SPREE TARPS TARTS
-CRASH TRASH SHARP HARPS CRUSH RUSTS
-RESTS STEER STARE TRACK THESE TRUCK
-`.split(/\s+/).filter( exists => !!exists );
-
-module.exports = wordList;
